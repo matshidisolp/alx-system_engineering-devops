682478c5b2f6882d4ae312d99d37ee7c034aed5b
matshidisolp <mpitswane@gmail.com>
2024-07-04 16:31:01 +0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
+# send request to given URL
+#displa the size of the body of the response
 
-# Check if the number of arguments is correct
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <URL>"
     exit 1
 fi
 
-# Assign URL from command-line argument
-url=$1
-
-# Use curl to fetch the URL and measure the size of the response body
-body_size=$(curl -s -o /dev/null -w '%{size_download}' "$url")
-
-# Print the size of the response body
-echo "$body_size"
+curl -s "$1" | wc -c
