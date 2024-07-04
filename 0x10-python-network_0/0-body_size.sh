c4484f13a3aeb82e558faf270d523c55f9ae68bf
matshidisolp <mpitswane@gmail.com>
2024-07-04 16:31:57 +0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 # send request to given URL
-#displa the size of the body of the response
-
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
+#display the size of the body of the response
 
 curl -s "$1" | wc -c
