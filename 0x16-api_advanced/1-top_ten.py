5b8de905f64b5ca8df510d96d3e66357395d01d4
matshidisolp <mpitswane@gmail.com>
2024-08-14 14:52:07 +0700

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-'''
+"""
 Queries the reddit api for number of subscribers
-'''
+"""
 import requests
 
 
 def top_ten(subreddit):
-    '''
+    """
     Get the number of subscribers from a subreddit
-    '''
+    """
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     header = {"User-Agent": "alx/1"}
     resp = requests.get(url, headers=header, allow_redirects=False)
